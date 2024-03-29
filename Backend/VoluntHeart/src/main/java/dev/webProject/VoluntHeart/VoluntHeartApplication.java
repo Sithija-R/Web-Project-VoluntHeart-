11ac1d310043a324561fce505a3528732f183071
Sithija R <Sithijarathukorale@gmail.com>
2024-03-29 09:54:44 +0530

    Project initialization

---
+++ Backend/VoluntHeart/src/main/java/dev/webProject/VoluntHeart/VoluntHeartApplication.java
@@ -0,0 +1,13 @@
+package dev.webProject.VoluntHeart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoluntHeartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoluntHeartApplication.class, args);
+	}
+
+}
