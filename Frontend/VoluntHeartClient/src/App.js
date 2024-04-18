6a4f7f2aae07525822197192e6bf8e058cbd9fc1
Sithija R <Sithijarathukorale@gmail.com>
2024-04-18 19:58:30 +0530

    18 april

--- Frontend/VoluntHeartClient/src/App.js
+++ Frontend/VoluntHeartClient/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Authentication from './Authentication/Authentication';
-import HomePage from './HomePage/HomePage';
+import HomePage from './Components/HomePage/HomePage';
 import {Route, Routes} from 'react-router-dom'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <div className="">
       
     <Routes>
-      <Route path="/" element={true?<HomePage/>:<Authentication/>}>
+      <Route path="/*" element={true?<HomePage/>:<Authentication/>}>
 
       </Route>
     </Routes>
